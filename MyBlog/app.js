52fe11e25031cb4ca83ee0c3b8c6e8daeec723d1
Nicholas March <n.march213@gmail.com>
2016-09-27 19:54:42 -0500

    Delete route added

--- MyBlog/app.js
+++ MyBlog/app.js
@@ -107,6 +107,19 @@ app.put("/blogs/:id", function(req, res){
 	})
 })
 
+//DELETE ROUTE
+app.delete("/blogs/:id", function(req, res){
+	//destroy blog
+	Blog.findByIdAndRemove(req.params.id, function(err){
+		if(err)
+		{
+			res.redirect("/blogs");
+		}else{
+			res.redirect("/blogs");
+		}
+	})
+	//redirect
+});
 
 
 
